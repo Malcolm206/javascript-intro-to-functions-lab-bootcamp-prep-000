73ff738ea56dc6f690137cde3892a5f5ed439fa5
Malcolm Katzenbach <mkatzenbach8@gmail.com>
2017-09-01 21:56:39 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -54,3 +54,9 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+function logShout(string) {
+  console.log('${string.toLowerCase()}')
+}
+function logWhisper(string) {
+  console.log('${string.toUpperCase()}')
+}
